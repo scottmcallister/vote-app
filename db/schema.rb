f43727bbd6015541deaadf5c1280f5c08d0c3b08
Scott McAllister <scottm@nextopia.com>
2016-08-07 14:16:27 +0000

    allow user to create polls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806200135) do
+ActiveRecord::Schema.define(version: 20160807140922) do
+
+  create_table "polls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
